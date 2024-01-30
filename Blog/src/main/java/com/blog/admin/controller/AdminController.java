8fcd7254dd8314015e918798e6bc91b11e837719
lfh <li_fenghe@outlook.com>
2024-01-30 14:00:40 +0800

    后台管理

--- Blog/src/main/java/com/blog/admin/controller/AdminController.java
+++ Blog/src/main/java/com/blog/admin/controller/AdminController.java
@@ -1,6 +1,6 @@
 package com.blog.admin.controller;
+import com.alibaba.fastjson.JSONObject;
 import com.blog.admin.entity.Result;
-import com.blog.admin.pojo.Omnibus;
 import com.blog.user.entity.StatusCode;
 import com.blog.user.pojo.Article;
 import com.blog.user.pojo.User;
@@ -8,6 +8,7 @@ import com.blog.user.service.ArticleService;
 import com.blog.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +30,16 @@ public class AdminController {
     @PostMapping(value = "/allArticles")
     public Result allArticles(){
         List<Article> data =  articleService.findAll();
+        return new Result<List<User>>(true, StatusCode.OK, "查询成功", data);
+
+    }
+    @PostMapping(value = "/comments")
+    public Result connemts(@RequestBody String data){
+        JSONObject jsonObject = JSONObject.parseObject(data);
+        Long userId = Long.parseLong(jsonObject.get("userId").toString());
+        Long articleId = Long.parseLong(jsonObject.get("articleId").toString());
+        String comment = jsonObject.get("comment").toString();
+        String date = jsonObject.get("date").toString();
         return new Result<List<User>>(true, StatusCode.OK, "查询成功", data);
 
     }
