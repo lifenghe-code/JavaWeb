6beb4a3d6847643136f548864b3fa8d43ee14507
lfh <li_fenghe@outlook.com>
2024-02-01 11:24:57 +0800

    添加sql语句

--- Blog/src/main/java/com/blog/admin/dao/AdminMapper.java
+++ Blog/src/main/java/com/blog/admin/dao/AdminMapper.java
@@ -1,7 +1,7 @@
 package com.blog.admin.dao;
-
 import com.blog.admin.pojo.Admin;
 import tk.mybatis.mapper.common.Mapper;
 @org.apache.ibatis.annotations.Mapper
 public interface AdminMapper  extends Mapper<Admin> {
+    Admin findAdminByName(String name);
 }
